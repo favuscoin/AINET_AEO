2fadf3390ccc9482e04ef522dbd023cfe9ff1efc
Michal <szachnom@Michals-MacBook-Pro.local>
2026-01-24 14:56:42 +0100

    Add cache-busting to logo images

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -45,7 +45,7 @@ export function Navbar() {
               {/* Logo */}
               <Link href="/" className="flex items-center">
                 <Image
-                  src={pathname === '/login' ? '/Union2.png' : '/Union1.png'}
+                  src={pathname === '/login' ? '/Union2.png?v=2' : '/Union1.png?v=2'}
                   alt="AIInternet"
                   width={120}
                   height={30}
